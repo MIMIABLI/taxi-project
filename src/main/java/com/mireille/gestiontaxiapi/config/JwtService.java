a43f1df90e9f00a57309ce05dfd241fb9f33196e
AmaxFR <amalaitagames@protonmail.com>
2023-03-06 14:35:18 +0100

    feat(Affichages listes) - Correction generation du token, fonctionne ok.

--- src/main/java/com/mireille/gestiontaxiapi/config/JwtService.java
+++ src/main/java/com/mireille/gestiontaxiapi/config/JwtService.java
@@ -44,8 +44,8 @@ public class JwtService {
         return Jwts.builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
-                .setIssuedAt(creationDate)
-                .setExpiration(expritationDate)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
